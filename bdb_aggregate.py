2566fc91a49b07b50a01a8415370d9fb116c3d44
harbingeroffire <harbingeroffire@proton.me>
2024-06-03 11:24:44 -0600

    Setup for stage 3

--- bdb_aggregate.py
+++ bdb_aggregate.py
@@ -11,3 +11,16 @@ def metadata(table):
 #True and False commands for auto commit
 ON=True
 OFF=False
+
+#aggregate functions for stage 3
+def COUNT(): pass
+def SUM(): pass
+def AVG(): pass
+def MIN(): pass
+def MAX(): pass
+def STDEV(): pass
+def STDEVP(): pass
+def MEDIAN(): pass
+def MODE(): pass
+def FIRST(): pass
+def LAST(): pass
