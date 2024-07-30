980fbaf8f4afc6b3f4e2fd0aa29dea3ad5155030
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-07-30 10:51:50 -0600

    well... the template didn't work...

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url="https://github.com/HarbingerOfFire/bivittatusDB",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.x",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
