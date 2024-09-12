b9039105ffe23d3ffe71c4b6fda15e3485391c2e
Unai Perez <unaipdd@gmail.com>
2024-09-12 20:13:27 +0200

    Menu little fix

--- BivittatusDB/menu.py
+++ BivittatusDB/menu.py
@@ -1,5 +1,5 @@
+from DB_manage import removing_rows
 from DB_manage.update_table import update_tb
-from DB_manage.removing_rows import remove_rows
 from DB_manage.adding_rows import add_names_to_db
 from DB_manage.table_view import use_table
 from DB_manage.metadata import print_metadata
@@ -32,7 +32,7 @@ def execute_option(option):
             add_names_to_db()
         elif option == "3":
             pause_and_clean(0)
-            remove_rows()
+            removing_rows()
         elif option == "4":
             pause_and_clean(0)
             update_tb()
