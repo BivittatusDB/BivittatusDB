1b52ff7c1a66548db81768de8f33d013d618e547
Unai Perez <unaipdd@gmail.com>
2024-09-15 18:57:53 +0200

    phase 2 update: externalisation of list_pydb()

--- BivittatusDB/DB_manage/update_table.py
+++ BivittatusDB/DB_manage/update_table.py
@@ -1,6 +1,7 @@
 import BivittatusDB as bdb
+from DB_manage.funtions.common.list_dir_pydb import list_pydb
 from DB_manage.funtions.common.saving import save_table
-from bdb_aggregate import pause_and_clean, delay, show
+from bdb_aggregate import pause_and_clean
 
 def get_input(prompt, valid_options=None, convert_func=None):
     while True:
@@ -52,57 +53,6 @@ def update_table(table):
             print(f"Error updating table: {e}")
             pause_and_clean(0.8)
 
-def list_pydb():
-    """
-    Prompts the user to enter the database folder name and table name, and initializes the database.
-    
-    Returns:
-        tuple: (db_directory, table_name) if successful, (None, None) if canceled or if an error occurs.
-    """
-    while True:
-        db_directory = input("Enter the name of the database folder (or 'n' to cancel): ").strip()
-
-        if db_directory.lower() == 'n':
-            print("Operation canceled.")
-            delay(0.8)
-            return None, None
-
-        try:
-            # Attempt to display the tables in the directory
-            tables = show(db_directory)
-            if not tables:
-                print(f"No tables found in directory '{db_directory}'.")
-                return None, None
-            break  # Exit loop if successful
-        except Exception as e:
-            print(f"Error retrieving tables from directory '{db_directory}': {e}")
-            delay(1)
-            continue  # Retry instead of returning None immediately
-
-    try:
-        # Initialize the database
-        db = bdb.database(db_directory).use()
-        print("Database initialized successfully.")
-    except Exception as e:
-        print(f"Error initializing the database: {e}")
-        return None, None
-
-    while True:
-        print("Available tables:", ", ".join(tables))
-        table_name = input("Enter the name of the table you want to use (without .pydb extension): ").strip()
-
-        if table_name not in tables:
-            pause_and_clean(0)
-            print(f"Table '{table_name}' not found. Please enter a valid table name from the list above.")
-        else:
-            try:
-                # Load the selected table
-                tb1 = db.load_table(table_name)
-                print(f"Table '{table_name}' loaded successfully.")
-                return db_directory, table_name
-            except Exception as e:
-                print(f"Error loading the table '{table_name}': {e}")
-                return None, None
 
 def update_tb():
     # Use list_pydb to select the database and table
