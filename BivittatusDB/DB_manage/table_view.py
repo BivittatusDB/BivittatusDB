c92f7ccd302b827baf94b59220c13bcd9ec2792e
Unai Perez <unaipdd@gmail.com>
2024-09-26 18:42:01 +0200

    refactor

--- BivittatusDB/DB_manage/table_view.py
+++ BivittatusDB/DB_manage/table_view.py
@@ -3,60 +3,44 @@ from bdb_aggregate import pause_and_clean, show, delay
 from DB_manage.funtions.common.user_interaction_common import get_db_choice
 
 def use_table():
-    # Get the user's choice (load existing DB or create a new one)
     db_choice = get_db_choice()
 
     if db_choice == "y":
-        db_directory = input("Enter the name of the database directory: ")
+        db_directory = input("Enter the name of the database directory: ").strip()
         
-        # Get the list of tables using the show function
         try:
             tables = show(db_directory)
-        except Exception as e:
-            print(f"Error retrieving tables: {e}")
-            delay(1.5)
-            return
-
-        try:
             db = bdb.database(db_directory).use()
             print("Database initialized successfully.")
         except Exception as e:
-            print(f"Error initializing the database: {e}")
+            print(f"Error: {e}")
+            delay(1.5)
             return
 
         while True:
-            # Show available tables
             print("Available tables:", ", ".join(tables))
+            table_name = input("Enter the name of the table you want to use (without .pydb extension): ").strip()
 
-            # Request the name of the table
-            table_name = input("Enter the name of the table you want to use (without .pydb extension): ")
-
-            # Check if the table exists
-            if table_name not in tables:
-                print(f"Table '{table_name}' not found. Please enter a valid table name from the list above.")
-            else:
+            if table_name in tables:
                 try:
-                    # Load the selected table
                     tb1 = db.load_table(table_name)
                     pause_and_clean(0)
                     print("The current table:")
                     print(tb1)
                     
-                    # Confirm exit
                     if confirm_exit(tb1):
-                        return True
-
-                    break  # Exit the loop if the table is loaded successfully
+                        return
                 except Exception as e:
                     print(f"Error loading the table '{table_name}': {e}")
+            else:
+                print(f"Table '{table_name}' not found. Please enter a valid table name from the list above.")
     else:
         print("Invalid choice. Exiting.")
-        return
 
 def confirm_exit(tb1):
     while True:
-        exit_choice = input("Do you want to exit? (y): ")
-        if exit_choice.strip() == 'y':
+        exit_choice = input("Do you want to exit? (y): ").strip().lower()
+        if exit_choice == 'y':
             pause_and_clean(0)
             return True
         else:
